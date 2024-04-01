76f496f525dd03da3d1f1c0c9207a0fae41fd6f2
LamNP123 <165629308+LamNP123@users.noreply.github.com>
2024-04-01 12:20:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  if (username === 'admin' && password === 'admin') {
+    window.location.href = 'csdl.html';
+  } else {
+    document.getElementById('errorMessage').innerText = 'Tài khoản hoặc mật khẩu không chính xác.';
+  }
+});
